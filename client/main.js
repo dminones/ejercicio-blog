0fa23d2b9f755b39a3d3bd0834a996d01984e0c8
DiegoHarari <diehara@gmail.com>
2019-05-22 21:49:47 -0300

    Issue #4 Creado.

--- client/main.js
+++ client/main.js
@@ -1,8 +1,19 @@
-console.log("It works");
-const servidor = "http://localhost:8080";
+const servidor = 'http://localhost:8080';
+const url_string = window.location.href;
+const url = new URL(url_string);
+const userName = url.searchParams.get('username');
 
-$.getJSON(servidor + "/posts", response => {
-  console.log(response);
-  const posts = response.posts;
-  $("#posts").html(`<ul>${posts.map(post => `<li>${post.titulo}</li>`)} </ul>`);
+$.getJSON(servidor + '/posts', (response) => {
+	const posts = response.posts;
+	$('#posts').html(`<ul>${posts.map((post) => `<li>${post.titulo}</li>`)} </ul>`);
+});
+
+$.getJSON(servidor + `/usuarios/${userName}`, (response) => {
+	const usuarios = response.nombre;
+	const imagen = response.imagen;
+	const usuariosBio =
+		'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, itaque. Commodi vero recusandae tempora hic architecto facilis quidem quas quibusdam?';
+	$('#usuarios').html(` <h3> ${usuarios} </h3>`);
+	$('#usuarios-bio').html(`<p> ${usuariosBio} </p>`);
+	$('#imagen').html(`<img src=${imagen}> `);
 });
