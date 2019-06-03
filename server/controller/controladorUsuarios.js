365241be5f16322096614abd4c26747a65efe8e5
DiegoHarari <diehara@gmail.com>
2019-06-02 21:36:34 -0300

    issue para crear users

--- server/controller/controladorUsuarios.js
+++ server/controller/controladorUsuarios.js
@@ -1,4 +1,5 @@
 var con = require("../conexion");
+
 function consulta(req, res) {
   var sql = "select * from usuarios";
 
@@ -16,11 +17,11 @@ function consulta(req, res) {
 
 function byUsername(req, res) {
   var sql = `select * from usuarios where username= '${req.params.username}'`;
-
+  
   con.query(sql, function(error, respuesta, fields) {
     if (error) {
       console.log("Hubo un error en la conexion", error.message);
-      return res.status(404).res.send("hubo un error en la conexion");
+      return res.status(404).send("hubo un error en la conexion");
     }
     if (respuesta && respuesta.length > 0) {
       const usuario = respuesta[0];
@@ -29,8 +30,26 @@ function byUsername(req, res) {
     return res.status(404);
   });
 }
+// FUNCTION PARA CREAR UN USUARIO
+function createUser(req, res) {
+  let sql = 'INSERT INTO facebook.usuarios (nombre, imagen, username, bio) VALUES (?,?,?,?)';
+  const data = req.body
+  con.query(sql,[data.nombre, data.imagen, data.userName, data.bio], function(error, respuesta, fields) {
+    if (error) {
+      console.log("Hubo un error en la conexion", error.message);
+      return res.status(404).res.send("hubo un error en la conexion");
+    }
+    if (respuesta && respuesta.length > 0) {
+      return res.send(JSON.stringify(respuesta));
+    }
+    return res.status(404);
+  });
+}
+
+
 
 module.exports = {
   consulta,
-  byUsername
+  byUsername,
+  createUser
 };
