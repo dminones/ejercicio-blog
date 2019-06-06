eae1fb5b8c3b69e420b97ca07bd04043e40e2d8f
ferdiaz93 <fernandodiaz1209@gmail.com>
2019-06-06 01:50:20 -0300

    definir errores

--- server/controller/controladorPosts.js
+++ server/controller/controladorPosts.js
@@ -22,12 +22,14 @@ function listarUnPost(req,res){
 
     if(error){
       return res.status(404).send(error)
-    };
-    if(resultado.lehgth == 0){
-      //falta poner el tipo de error
-    }else{
-      res.send(JSON.stringify(resultado))
     }
+
+    let response = {
+      post: resultado
+    };
+
+    res.send(JSON.stringify(response))
+
   })
   
 
